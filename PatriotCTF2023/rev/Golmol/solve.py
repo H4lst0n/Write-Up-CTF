27e949dddee3940ec73c11ba82dd20ce4aa7bfdb
HaiND <91616280+H4lst0n@users.noreply.github.com>
2023-09-12 01:46:47 +0700

    Delete PatriotCTF2023/rev/Golmol directory

--- PatriotCTF2023/rev/Golmol/solve.py
+++
@@ -1,9 +0,0 @@
-from pwn import *
-
-for i in range(100):
-    p = process("./ok")
-    p.recvuntil(b"++++++++++++++++++++")
-    p.recvuntil(b"Secret Please:")
-    p.recvuntil(b"++++++++++++++++++++")
-    p.send(str(i))
-    p.interactive()    
